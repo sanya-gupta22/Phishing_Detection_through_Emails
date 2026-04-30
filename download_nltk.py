5ff763a00cdfbf883fc082827dca1593aed12b55
sanya-gupta22 <sanyagupta478@gmail.com>
2026-04-30 19:43:02 +0530

    update

--- download_nltk.py
+++ download_nltk.py
@@ -1,5 +1,12 @@
 import nltk
+import os
 
-nltk.download('wordnet', download_dir='./nltk_data')
-nltk.download('omw-1.4', download_dir='./nltk_data')
-nltk.download('stopwords', download_dir='./nltk_data')
+download_path = "/opt/render/nltk_data"
+
+print("Downloading NLTK data to:", download_path)
+
+nltk.download('stopwords', download_dir=download_path)
+nltk.download('wordnet', download_dir=download_path)
+nltk.download('omw-1.4', download_dir=download_path)
+
+print("Download complete")
